d144d9b4dffbec853bad3c38761b0126a4f26acc
Marta <marta.klaudia.nowak@gmail.com>
2020-02-01 13:54:10 +0100

    Updates

---
+++ precache-manifest.c46c7c97ed2c37c36fef6b0e57966aee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5c4576d98d00dc733a8b6bf8d441ac",
+    "url": "/save-water-app/index.html"
+  },
+  {
+    "revision": "9b7746f26e874420a6ca",
+    "url": "/save-water-app/static/css/main.9628c3d3.chunk.css"
+  },
+  {
+    "revision": "2adf474b0fe85f9473ca",
+    "url": "/save-water-app/static/js/2.57c6d613.chunk.js"
+  },
+  {
+    "revision": "b4693211b70fb081e85e1940ef60e919",
+    "url": "/save-water-app/static/js/2.57c6d613.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b7746f26e874420a6ca",
+    "url": "/save-water-app/static/js/main.8bbf52f7.chunk.js"
+  },
+  {
+    "revision": "ec39549fe06ce54c75f6",
+    "url": "/save-water-app/static/js/runtime-main.2911ff10.js"
+  },
+  {
+    "revision": "87debb6c80f61af0a9eed19f624342a1",
+    "url": "/save-water-app/static/media/rain-4559068_1280.87debb6c.jpg"
+  },
+  {
+    "revision": "f366757892aa3adedcd80cb7e2429c76",
+    "url": "/save-water-app/static/media/rain-4559068_640.f3667578.jpg"
+  }
+]);
